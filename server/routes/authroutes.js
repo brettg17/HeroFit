bd8ceb056aea0a7ea55db8f986b61c806b87089b
rhuynh17 <rhuynh1106@gmail.com>
2024-07-25 03:08:07 -0600

    created routes in server side

---
+++ server/routes/authroutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+
+// Register Route
+
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, password]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//Login Route
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1 AND password = $2",
+      [email, password]
+    );
+    if (result.rows.length > 0) {
+      res.status(200).json(result.rows[0]);
+    } else {
+      res.status(401).json({ error: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Home Route
+
+router.get("/", (req, res) => {
+  res.send("Hello, welcome to the FitApp!");
+});
+
+module.exports = router;
