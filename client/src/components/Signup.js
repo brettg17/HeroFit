d570716fcb04e28f6adf5b142f352d6abaebf44e
brettg17 <brettgelinas7@gmail.com>
2024-07-24 22:28:54 -0600

    created signup form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,5 +1,24 @@
+import '../styles/Signup.css';
+
 function Signup() {
-  return <h2>Signup</h2>;
+  return (
+    <div className="signup-container">
+      <div className="signup-box">
+        <h2> Sign Up</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input type="email" id="email" placeholder="Enter your email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter your password" />
+          </div>
+          <button type="submit" className="signup-button">Signup</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Signup;
