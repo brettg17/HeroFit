874919405b9ab0b4503abc7ee86613a36eda5893
brettg17 <brettgelinas7@gmail.com>
2024-08-02 20:27:20 -0600

    issue pushing to github hopefully resolved?

--- server/index.js
+++ server/index.js
@@ -2,28 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
 
-// import routes
-const homeRoute = require("./routes/homeRoute");
-const loginRoute = require("./routes/loginRoute");
-const signupRoute = require("./routes/signupRoute");
-const workoutRoute = require('./routes/workouts'); 
+// Import routes
+const homeRoute = require('./routes/homeRoute');
+const loginRoute = require('./routes/loginRoute');
+const signupRoute = require('./routes/signupRoute');
+const workoutRoute = require('./routes/workouts');
+const xpRoute = require('./routes/xpRoute');
 const dailyChallengesRoute = require('./routes/dailyChallengesRoute');
 
-
 const app = express();
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-//ROUTES
-app.use("/api/home", homeRoute);
-app.use("/api/auth", loginRoute);
-app.use("/api/auth", signupRoute);
+// Routes
+app.use('/api/home', homeRoute);
+app.use('/api/auth', loginRoute);
+app.use('/api/auth', signupRoute);
 app.use('/api/workouts', workoutRoute);
+app.use('/api/xp', xpRoute);
 app.use('/api/daily-challenges', dailyChallengesRoute);
-
 
 const PORT = process.env.PORT || 5001;
 
